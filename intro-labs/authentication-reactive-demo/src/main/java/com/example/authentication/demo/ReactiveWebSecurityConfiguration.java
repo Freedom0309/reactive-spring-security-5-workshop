f9886125928190d2929efe340295b0d112bca842
liufeng <15332622786@163.com>
2020-10-26 18:33:46 +0800

    lab1添加默认用户及密码

--- intro-labs/authentication-reactive-demo/src/main/java/com/example/authentication/demo/ReactiveWebSecurityConfiguration.java
+++ intro-labs/authentication-reactive-demo/src/main/java/com/example/authentication/demo/ReactiveWebSecurityConfiguration.java
@@ -5,20 +5,33 @@ import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
 public class ReactiveWebSecurityConfiguration {
 
-  @Bean
-  SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-    return http.authorizeExchange()
-            .matchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
-            .anyExchange()
-            .authenticated()
-            .and()
-            .httpBasic().and().formLogin()
-            .and().build();
-  }
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange()
+                .matchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .httpBasic().and().formLogin()
+                .and().build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new MapReactiveUserDetailsService(user);
+    }
 
 }
